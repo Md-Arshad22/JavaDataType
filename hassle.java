e5ebf8ebf5a04d8a4ec052fcfd77622e2398fab4
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-08-05 23:50:22 +0530

    Update hassle.java

--- hassle.java
+++ hassle.java
@@ -1,3 +1,10 @@
-public static Steadily 
-
+public class Steadily {
+  public static void main(String[] args) {
+      String str = "Arshad";
+      int a = 2000;
+      float ft = 20.541f;
+      double db = 20015.451d;
+      System.out.println(str + " " + a + " " + ft + " " + db);
+  }
+}
 
